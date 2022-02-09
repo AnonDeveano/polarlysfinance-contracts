e4609363b96ef56fc8bdf935601e4f67ed40016b
DevGutta <dev_attug@protonmail.com>
2022-02-09 06:56:15 -0500

    Storage.sol added

---
+++ contracts/Storage.sol
@@ -0,0 +1,23 @@
+pragma solidity ^0.5.0;
+
+contract Storage {
+
+    mapping (string => string) private _store;
+
+    function addData(string memory key, string memory value) public {
+        require(bytes(_store[key]).length == 0);
+        _store[key] = value;
+    }
+
+    function removeData(string memory key) public returns (string memory) {
+        require(bytes(_store[key]).length != 0);
+        string memory prev = _store[key];
+        delete _store[key];
+        return prev;
+    }
+
+    function changeData(string memory key, string memory newValue) public {
+        require(bytes(_store[key]).length != 0);
+        _store[key] = newValue;
+    }
+}
