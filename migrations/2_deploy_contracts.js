9ae3dc696dc9fa863d51f3496eb8cbe7b05da27d
DevGutta <dev_attug@protonmail.com>
2022-02-25 07:05:58 -0500

    Working on deployments to Aurora testnet

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -30,13 +30,19 @@ module.exports = function (deployer) {
     const _devFund = "0x7299192CD862c9c5345cC47a2Ef24807436009b0";
     const _communityFund = "0x86A247546cA84735542bF61BEE722b0250bDFfc9";
     const _teamFund = "0x5A7a3609474790cb6399b5F0422967e995037A1d";
+    const admin_ = "0x7299192CD862c9c5345cC47a2Ef24807436009b0";
 
-    // this is the local contract address of Nebula.sol, just testing deployments
-    // so this is NOT a valid pair
-    const _pair = "0x9dECB539Ac4B4DE5984d8f555907fAF17c57272d";
-    // local contract address of Borealis/Stardust just so it can be tested in TaxOfficeV2
-    const _base = "0xd103Ed8CE77d003B9f203183dB2490eC7384e855";
-    const _router = "0x641FB670D5328B2c5c294Eef8A40c0676ED6fd4c";
+    // define these explicitly as required for TaxOfficeV2
+    const _pair = "";
+    const _base = "";
+    const _router = "";
+
+    // nebula testnet address
+    const _nebula = "";
+    // boralis testnet address
+    const _borealis = "";
+    // required for contracts under distributions/
+    const poolStartTime = "";
 
     let _distributors = [];
 
@@ -48,17 +54,23 @@ module.exports = function (deployer) {
     deployer.deploy(Nebula);
     deployer.deploy(SimpleERCFund);
     deployer.deploy(StarDust);
-    deployer.deploy(TaxOffice, _pair); //
-    deployer.deploy(TaxOfficeV2, _pair, _base, _router);
-    deployer.deploy(TaxOracle, _pair, _base, _router);
-    deployer.deploy(Timelock, _devFund, delay);
+    // requires Nebula address
+    deployer.deploy(TaxOffice);
+    // requires Nebula address, base, and router. WHAT IS BASE AND ROUTER explicitly?
+    deployer.deploy(TaxOfficeV2);
+    // requires Nebula, near, and pair in constructor but contains functions to set later
+    deployer.deploy(TaxOracle);
+    deployer.deploy(Timelock, admin_, delay);
+    // Does this need to be deployed with params?
     deployer.deploy(Treasury);
     deployer.deploy(WarpDrive);
-    // using other vars from above just to test deployment
-    deployer.deploy(BorealisRewardPool, _base, time);
-    deployer.deploy(NebulaGenesisRewardPool, _pair, _router, time);
-    deployer.deploy(NebulaRewardPool, _pair, time);
-    // moved to the end of this deploy stack since require statement is reverting 
-    // due to no locked liq for this test
-    deployer.deploy(Oracle, _pair, _period, time);
+    // requires Borealis var and Pool start time
+    deployer.deploy(BorealisRewardPool);
+    // what is the shiba const referring to?
+    // requires nebula address, shiba address, and can use same pool start time as Borealis
+    deployer.deploy(NebulaGenesisRewardPool);
+    // requires Nebula address and pool start time
+    deployer.deploy(NebulaRewardPool);
+    // requires pair, period, start params
+    deployer.deploy(Oracle);
 };
