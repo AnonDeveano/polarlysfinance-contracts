c919fd7b44dcd44b9da9f1de3543ca08ed3ef006
DevGutta <dev_attug@protonmail.com>
2022-03-15 06:40:48 -0400

    Updated contract name

--- contracts/interfaces/IWrappedNear.sol
+++ contracts/interfaces/IWrappedNear.sol
@@ -4,7 +4,7 @@ pragma solidity 0.6.12;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-interface IWrappedETH is IERC20 {
+interface IWrappedNear is IERC20 {
     function deposit() external payable returns (uint256);
 
     function withdraw(uint256 amount) external returns (uint256);
