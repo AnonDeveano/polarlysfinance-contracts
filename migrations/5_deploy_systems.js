147964547b93dd527ce5bc9e61c31d3629fb7586
DevGutta <dev_attug@protonmail.com>
2022-04-05 06:56:52 -0400

    Testnet deploy success

--- migrations/5_deploy_systems.js
+++ migrations/5_deploy_systems.js
@@ -7,22 +7,26 @@ const Treasury = artifacts.require('Treasury');
 module.exports = async (deployer) => {
     const nebula = "0xF1FA1E50418c2B4c6ea8669101053Ddb304E9e1e";
 
-    // this might fail with require statement that nebula_pair =/= 0
-    const nebula_pair = "0x0000000000000000000000000000000000000000";
+    // LP added on Aries testnet
+    const nebula_pair = "0x9F175EFB10915dFc523c6ce36593301A2e408be7";
 
     const wnear = "0xC42C30aC6Cc15faC9bD938618BcaA1a1FaE8501d";
 
-    const router = '0x0000000000000000000000000000000000000000';
+    // Personal address used just to deploy
+    const router = '0x7299192CD862c9c5345cC47a2Ef24807436009b0';
 
-    const oracle_start_time = '0';
+    const oracle_start_time = 1659069176;
+
+    // coped from Polaris Finance just to see test deploy
+    const PERIOD = 2600;
 
     // TaxOracle, can set Nebula, wnear, and pair after
     await deployer.deploy(TaxOracle, nebula, wnear, nebula_pair);
 
     // Treasury
     await deployer.deploy(Treasury);
-    const treasury = await Treasury.deployed();
-    const PERIOD = await treasury.PERIOD();
+    // const treasury = await Treasury.deployed();
+    // const PERIOD = await treasury.PERIOD();
 
     // Oracle
     await deployer.deploy(Oracle, nebula_pair, PERIOD, oracle_start_time);
@@ -31,5 +35,5 @@ module.exports = async (deployer) => {
     await deployer.deploy(WarpDrive);
 
     // TaxOfficeV2
-    await deployer.deploy(TaxOfficeV2, nebula, router);
+    await deployer.deploy(TaxOfficeV2, nebula, wnear, router);
 }
