33aaa04affc955eb5dbf4dbfcbaa9534f5bf2533
DevGutta <dev_attug@protonmail.com>
2022-04-06 07:24:38 -0400

    Updating migrations

--- migrations/5_deploy_systems.js
+++ migrations/5_deploy_systems.js
@@ -1,8 +1,5 @@
 const TaxOracle = artifacts.require('TaxOracle');
-const Oracle = artifacts.require('Oracle');
 const TaxOfficeV2 = artifacts.require('TaxOfficeV2');
-const WarpDrive = artifacts.require('WarpDrive');
-const Treasury = artifacts.require('Treasury');
 
 module.exports = async (deployer) => {
     const nebula = "0xF1FA1E50418c2B4c6ea8669101053Ddb304E9e1e";
@@ -15,24 +12,11 @@ module.exports = async (deployer) => {
     // Personal address used just to deploy
     const router = '0x7299192CD862c9c5345cC47a2Ef24807436009b0';
 
-    const oracle_start_time = 1659069176;
-
-    // coped from Polaris Finance just to see test deploy
     const PERIOD = 2600;
 
     // TaxOracle, can set Nebula, wnear, and pair after
     await deployer.deploy(TaxOracle, nebula, wnear, nebula_pair);
 
-    // Treasury
-    await deployer.deploy(Treasury);
-    // const treasury = await Treasury.deployed();
-    // const PERIOD = await treasury.PERIOD();
-
-    // Oracle
-    await deployer.deploy(Oracle, nebula_pair, PERIOD, oracle_start_time);
-
-    // WarpDrive
-    await deployer.deploy(WarpDrive);
 
     // TaxOfficeV2
     await deployer.deploy(TaxOfficeV2, nebula, wnear, router);
